a409c32e4f181be216d5370e838ffb8af91db014
Justin Gallagher <justingallag@gmail.com>
2014-01-06 22:58:16 -0500

    Changed to an external script.

--- horizontalscroll.js
+++ horizontalscroll.js
@@ -1,21 +1,59 @@
-var scrollHorizontal = function (e) {
-	var event = e || window.event;
-	if (document.body.doScroll){
-		// Internet Explorer
-		document.body.doScroll(event.wheelDelta > 0 ? "left":"right");
-	} else if ((event.wheelDelta || event.detail) > 0){
-		document.body.scrollLeft -= 40;
-	} else {
-		document.body.scrollLeft += 40;
+/* Copyright 2008 Paul Bennett - http://paulicio.us
+ * Scroller.js
+ * Captures mouse wheel events and runs the ScrollSmoothly
+ * function based on their output.
+ * Aims to aid usability by allowing the user to scroll the 
+ * page horizontally smoothly using only their mousewheel.
+ * Mousewheel event capture by Adomas Paltanavičius at http://adomas.org/
+ */
+
+function handle(delta) {
+        if (delta <0)
+                ScrollSmoothly(10,10,'right');
+        else if (delta >0)
+                ScrollSmoothly(10,10,'left');
+        else
+        	;
+}
+ 
+function wheel(event){
+        var delta = 0;
+        if (!event) 
+                event = window.event;
+        if (event.wheelDelta) {
+                delta = event.wheelDelta/120;
+                if (window.opera)
+                        delta = -delta;
+        } else if (event.detail) {
+                delta = -event.detail/3;
+        }
+        if (delta)
+                handle(delta);
+        if (event.preventDefault)
+                event.preventDefault();
+	event.returnValue = false;
+}
+
+var repeatCount = 0;
+
+function ScrollSmoothly(scrollPos,repeatTimes, direction) {
+	if(repeatCount < repeatTimes)
+		if(direction == 'right')
+			window.scrollBy(20,0);
+		else
+			window.scrollBy(-20,0);
+	else
+	{
+		repeatCount = 0;
+		clearTimeout(cTimeout);
+		return;
 	}
-
-	return false;
+	repeatCount++;
+	cTimeout = setTimeout("ScrollSmoothly('" + scrollPos + "','"+ repeatTimes +"','"+ direction +"')",10);
 }
-
-if ("onmousewheel" in document.body){
-	// Most browsers
-	document.body.onmousewheel = scrollHorizontal;
-} else {
-	// Firefox
-	document.body.addEventListener("DOMMouseScroll", scrollHorizontal);
-}
+ 
+/* Initialization code. */
+if (window.addEventListener)
+        window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+ 
